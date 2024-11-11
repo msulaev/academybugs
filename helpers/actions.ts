03ee904fd18745a22a96123bf1a3899ab9f91b73
msulaev <msulaev@users.noreply.github.com>
2024-11-11 12:39:12 +0000

    deploy: 2260cb2b7c60362c25eef1c6112d7f50165f206f

--- helpers/actions.ts
+++
@@ -1,8 +0,0 @@
-import { Locator, Page, test } from '@playwright/test';
-
-export async function clickTo(page: Page, locator: Locator): Promise<void> {
-    const description = `Click to element: ${locator}`;
-    await test.step(description, async () => {
-        await locator.click();
-    });
-}
--- types/currency.ts
+++
@@ -1,8 +0,0 @@
-export const Currency = {
-    JPY: 'JPY',
-    GBP: 'GBP',
-    EUR: 'EUR',
-    USD: 'USD',
-} as const;
-
-export type Currency = typeof Currency[keyof typeof Currency];
