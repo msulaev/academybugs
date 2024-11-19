f5ca42ef36fb5d5ef71d8274b98f153245226e04
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-11-19 14:49:50 +0200

    Change fixture, add allureMeta

--- fixtures/fixture.ts
+++ fixtures/fixture.ts
@@ -1,15 +1,25 @@
 import { test as base } from '@playwright/test';
 import { PageManager } from '../pages/PageManager';
+import { epic, feature } from 'allure-js-commons';
 
 type Fixtures = {
   pm: PageManager;
+  allureMeta: { epic: string; feature: string };
 };
 
 export const test = base.extend<Fixtures>({
-  pm: async ({ page }, use) => {
+  pm: async ({ page, allureMeta }, use) => {
+    if (allureMeta.epic) {
+      epic(allureMeta.epic);
+    }
+    if (allureMeta.feature) {
+      feature(allureMeta.feature);
+    }
     const pm = new PageManager(page);
     await use(pm);
   },
+
+  allureMeta: [{ epic: '', feature: '' }, { option: true }],
 });
 
 export { expect } from '@playwright/test';
