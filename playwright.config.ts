207400d5a8d566b643a2520fd1ba4a35afa73987
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-12-26 16:24:26 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json'], ['allure-playwright']],
+  reporter: [
+    ['allure-playwright'],
+    ['json', { outputFile: 'test-results.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
